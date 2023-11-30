067d3c03eab55236bdbd7ecfe382c3c5ade79e56
Artur Maslov <aartasas@gmail.com>
2023-11-30 22:10:30 +0200

    increase major version number with finished ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.arturmaslov.skubaware"
         minSdk = 25
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.4"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
